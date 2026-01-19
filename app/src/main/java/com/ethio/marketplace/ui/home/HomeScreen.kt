b1790223e4c4b4e4b418446910f5bf240a8b4caf
go-mark8 <mktechs02@gmail.com>
2026-01-19 10:50:04 -0800

    Last Sync: 2026-01-19 10:50 (Mobile)

---
+++ app/src/main/java/com/ethio/marketplace/ui/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+
+package com.ethio.marketplace.ui.home
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
+@Composable
+fun HomeScreen() {
+ Text("ETHIO🛍 Marketplace Ready", style = MaterialTheme.typography.headlineMedium)
+}
---
+++ app/src/main/java/com/ethio/marketplace/ui/listing/ListingDetailScreen.kt
@@ -0,0 +1,1 @@
+// ListingDetailScreen placeholder
