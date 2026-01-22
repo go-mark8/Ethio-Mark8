73a95e2e7d257caa28674fff9f06cb80cd167fa1
go-mark8 <mktechs02@gmail.com>
2026-01-22 09:12:02 -0800

    Last Sync: 2026-01-22 09:11 (Mobile)

--- hope /android/app/src/main/kotlin/com/trizy/trizy_app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.trizy.trizy_app
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
