d6c287d67ae1dd630c29fb77a5e7df889141608f
Ethio <mktechs02@gmail.com>
2026-03-06 10:31:01 -0800

    Create Dev.nix

---
+++ Dev.nix
@@ -0,0 +1,52 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+      "Nash.awesome-flutter-snippets"
+      "usernamehw.errorlens"
+    ];
+
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        build-flutter = "cd /workspace && flutter pub get";
+      };
+      # Runs when the workspace starts
+      onStart = {
+        # Run build_runner for generated files (Freezed, Retrofit, Riverpod)
+        run-build-runner = "flutter pub run build_runner build --delete-conflicting-outputs";
+      };
+    };
+
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
+      };
+    };
+  };
+}
